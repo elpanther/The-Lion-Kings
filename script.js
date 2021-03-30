7bd0507d560da018ffa18c904d0708574387d5ea
Jerrified <68245366+Jerrified@users.noreply.github.com>
2021-03-30 17:44:05 -0400

    Added auto-expanding textarea

---
+++ script.js
@@ -0,0 +1,27 @@
+// Auto-extending textarea
+
+// NOTE: The textarea only expands as far as the page is able to go, so the scroll bar will appear after a couple hundred characters.
+
+document.addEventListener('input', function (event) {
+	if (event.target.tagName.toLowerCase() !== 'textarea') return;
+	autoExpand(event.target);
+}, false);
+
+var autoExpand = function (field) {
+
+	// Reset field height
+	field.style.height = 'inherit';
+
+	// Get the computed styles for the element
+	var computed = window.getComputedStyle(field);
+
+	// Calculate the height
+	var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+	             + parseInt(computed.getPropertyValue('padding-top'), 10)
+	             + field.scrollHeight
+	             + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+	             + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+	field.style.height = height + 'px';
+
+};
