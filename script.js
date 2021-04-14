7c47da939f58da4a110c6f947b3227e35eacf8d5
Jerrified <68245366+Jerrified@users.noreply.github.com>
2021-04-14 18:19:10 -0400

    Added jquery

--- script.js
+++ script.js
@@ -27,3 +27,11 @@ var autoExpand = function (field) {
 };
 
 // Pop up form
+$(document).ready(function() {
+
+	// Initialize the plugin
+	$('#add-new-item').popup();
+
+	// Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
+	// $.fn.popup.defaults.pagecontainer = '#page'
+  });
