a6f4e65766d6db6eae8ab3b2ef6c08e8d43d1b6c
Route9Library <you@example.com>
2021-05-06 18:30:46 -0400

    resources

--- script.js
+++ script.js
@@ -183,33 +183,6 @@ function changeImage(imgName)
      image.src = imgName;
   }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("Howto");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-Howto.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 var selectedRow = null
 
 function onFormSubmit() {
@@ -338,3 +311,6 @@ function onFormSubmit() {
     }
 }
 
+function emailValidation() {
+
+}
