3016c695ca1279ca437bc1d1a8bc825b5df96d91
Jerrified <68245366+Jerrified@users.noreply.github.com>
2021-05-18 17:24:29 -0400

    attempts to save item form inputs

--- script.js
+++ script.js
@@ -394,12 +394,12 @@ window.onbeforeunload = function () {
 //     var userPw = document.getElementById('psw');
 
 
-    // check if stored data from register-form is equal to data from login form
-    if(userName.value !== storedName || userPw.value !== storedPw) {
-        alert('ERROR');
-    }else {
-        alert('You are loged in.');
-    }
+// check if stored data from register-form is equal to data from login form
+if (userName.value !== storedName || userPw.value !== storedPw) {
+  alert('ERROR');
+} else {
+  alert('You are loged in.');
+}
 
 
 
@@ -445,26 +445,29 @@ window.onbeforeunload = function () {
 // }
 
 //   localStorage.setItem("table_info", field);
-  // Retrieve
+// Retrieve
 
-  document.getElementById("name").value = getSavedValue("name");    // set the value to this input
-  document.getElementById("purpose").value = getSavedValue("purpose");   // set the value to this input
-  document.getElementById("amount").value = getSavedValue("amount");   // set the value to this input
-  document.getElementById("category").value = getSavedValue("category");   // set the value to this input
-  document.getElementById("description").value = getSavedValue("description");   // set the value to this input
-  /* Here you can add more inputs to set value. if it's saved */
+document.getElementById("name").value = getSavedValue("name");    // set the value to this input
+document.getElementById("purpose").value = getSavedValue("purpose");   // set the value to this input
+document.getElementById("amount").value = getSavedValue("amount");   // set the value to this input
+document.getElementById("category").value = getSavedValue("category");   // set the value to this input
+document.getElementById("description").value = getSavedValue("description");   // set the value to this input
+/* Here you can add more inputs to set value. if it's saved */
 
-  //Save the value function - save it to localStorage as (ID, VALUE)
-  function saveValue(e){
-      var id = e.id;  // get the sender's id to save it . 
-      var val = e.value; // get the value. 
-      localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+//Save the value function - save it to localStorage as (ID, VALUE)
+function saveValue(e) {
+  var id = e.id;  // get the sender's id to save it . 
+  var val = e.value; // get the value. 
+  localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+}
+
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue(v) {
+  if (!localStorage.getItem(v)) {
+    return "";// You can change this to your defualt value. 
   }
-
-  //get the saved value function - return the value of "v" from localStorage. 
-  function getSavedValue  (v){
-      if (!localStorage.getItem(v)) {
-          return "";// You can change this to your defualt value. 
-      }
-      return localStorage.getItem(v);
-  }
+  return localStorage.getItem(v);
+}
+window.beforeonload() = function () {
+  getSavedValue(this);
+};
